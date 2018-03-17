ceec6a13d031febcf8b8818515cf4b19fdb222c6
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-17 22:47:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,12 +8,3 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(5)
-  number = 10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end
